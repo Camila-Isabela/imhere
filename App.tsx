6a486e18082ec39acf0b5ad064a8c563b79d853b
Camila-Isabela <camila_isabela_lins@hotmail.com>
2022-10-10 16:07:02 -0300

    First steps

--- App.tsx
+++ App.tsx
@@ -2,9 +2,31 @@ import { Text, View } from "react-native"
 
 export default function App() {
 	return (
-		<View>
-			<Text>Nada</Text>
-			<Text>Nada</Text>
+		<View
+			style={{
+				flex: 1,
+				backgroundColor: "#131016",
+				padding: 24,
+			}}
+		>
+			<Text
+				style={{
+					color: "#FDFCFE",
+					fontSize: 24,
+					fontWeight: "bold",
+					marginTop: 48,
+				}}
+			>
+				Nome do evento
+			</Text>
+			<Text
+				style={{
+					color: "#6B6B6B",
+					fontSize: 16,
+				}}
+			>
+				Segunda, 10/10/2022
+			</Text>
 		</View>
 	)
 }
