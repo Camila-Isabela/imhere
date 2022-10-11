3621aa42a6207082d83d79031ae59b4e9ea10037
Camila-Isabela <camila_isabela_lins@hotmail.com>
2022-10-11 10:49:20 -0300

    button, input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,25 +1,28 @@
-import { Text, View } from "react-native"
+import { Text, View, TextInput, TouchableOpacity } from "react-native"
 import { styles } from "./styles"
 
 export function Home() {
+	function handleParticipantAdd() {
+		console.log("Você clicou no botão de adicionar!")
+	}
+
 	return (
 		<View style={styles.container}>
-			<Text
-				style={styles.eventName}
-			>
-				Aula de PDM
-			</Text>
-			<Text
-				style={styles.eventDate}
-			>
-				Segunda, 10/10/2022
-			</Text>
-      <Text style={styles.eventInfo}>
-        Aplicação para gerenciar os participantes presentes em eventos, como palestras, workshops, debates...
-      </Text>
-      <Text style={styles.eventRed}>
-        (Em construção)
-      </Text>
+			<Text style={styles.eventName}>Nome do Evento</Text>
+			<Text style={styles.eventDate}>Terça, 11/10/2022</Text>
+			<View style={styles.form}>
+				<TextInput
+					style={styles.input}
+					placeholder='Nome do Participante'
+					placeholderTextColor='#6B6B6B'
+				/>
+				<TouchableOpacity
+					style={styles.button}
+					onPress={handleParticipantAdd}
+				>
+					<Text style={styles.buttonText}>+</Text>
+				</TouchableOpacity>
+			</View>
 		</View>
 	)
 }
