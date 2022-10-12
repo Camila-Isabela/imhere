b89b2fc990e7af8be123e702db935c1e871c4d45
Camila-Isabela <camila_isabela_lins@hotmail.com>
2022-10-12 17:10:11 -0300

    Readme

---
+++ README.md
@@ -0,0 +1,59 @@
+<h1 align="center" style="font-size:3.4rem">I'm Here<h1>
+
+<h1 align="center">
+<img src="./assets/1.jpeg" width="100" height="100" style="align-items: center"><br>
+</h1>
+
+<h2 align="center">
+<a href="#about">About</a>  |
+<a href="#status">Status</a>  |
+<a href="#preview">Preview</a>  |
+<a href="#technologies">Technologies</a>  |
+<a href="#contribute">Contribute</a>  | 
+</h2>
+
+<h2 id="about">🏷 About The Project</h2>
+<p>Application to manage attendees at events such as lectures and workshops</p>
+
+<h2 id="status"> 🚦Status </h2>
+<h4 align="center"> 
+	🚀 Done! Waiting for features...  🚧
+</h4>
+
+### Goals
+
+- [x] FlatList x ScrollView
+- [x] StatusBar
+- [x] Add item
+- [x] Remove item
+- [x] Breakpoints
+- [x] Debugger
+- [x] UseState
+- [x] Immutability
+
+<h2 id="preview">🔎 Preview </h2>
+<div text-align="center">      
+<h3>AppMobile</h3>
+<img src="./assets/1.jpeg" width="200">
+<img src="./assets/2.jpeg" width="200">
+<img src="./assets/3.jpeg" width="200">
+<img src="./assets/4.jpeg" width="200">
+</div>
+
+<h2 id="technologies">🧰 Used Technologies </h2>
+
+<img src="./assets/react.png" width="100" height="75" style="border-radius:20%">&nbsp;&nbsp;&nbsp;
+<img src="./assets/javascript.png" width="100" height="75" style="border-radius:20%">&nbsp;&nbsp;&nbsp;
+<img src="./assets/typescript.png" width="100" height="75" style="border-radius:20%">&nbsp;&nbsp;&nbsp;
+<img src="./assets/expo.png" width="100" height="75" style="border-radius:20%">&nbsp;&nbsp;&nbsp;<br><br>
+
+<h2 id="contribute">🖇 Contribute</h2>
+
+    💡 Explore your creativity and skills
+    📥 Make a fork
+    🔱 Create a branck with your feature
+    〽 Commit changes
+    💠 Make a push to your branch
+
+<h3>&nbsp;That's it! 🖖</h3>
+<p>Try and miss again until its works.&nbsp;<img src="./assets/footlogo.png" alt="logotipo footer"></p>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,44 +1,58 @@
-import React, { useState } from 'react'
-import { Text, View, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native'
+import React, { useState } from "react"
+import {
+	Text,
+	View,
+	TextInput,
+	TouchableOpacity,
+	FlatList,
+	Alert,
+} from "react-native"
 import { Participant } from "../../components/Participant"
 import { styles } from "./styles"
 
 export function Home() {
-
-	const [participants, setParticipants] = useState(['Jack'])
+	const [participants, setParticipants] = useState<string[]>([])
+	const [participantName, setParticipantName] = useState("")
 
 	function handleParticipantAdd() {
-		if(participants.includes('Rodrigo')) {
-			Alert.alert('Participante existe', 'Já existe um participante na lista com esse nome.')
+		if (participants.includes(participantName)) {
+			return Alert.alert(
+				"Participante existe",
+				"Já existe um participante na lista com esse nome."
+			)
 		}
 
-		setParticipants(prevState => [...prevState, 'Adriana'])
-
+		setParticipants((prevState) => [...prevState, participantName])
+		setParticipantName("")
 	}
 
 	function handleParticipantRemove(name: string) {
-
-		Alert.alert('Remover', `Remover o participante ${name}?`, [
+		Alert.alert("Remover", `Remover o participante ${name}?`, [
 			{
-				text: 'Sim',
-				onPress: () => Alert.alert('Removido!')
+				text: "Sim",
+				onPress: () =>
+					setParticipants((prevState) =>
+						prevState.filter((participant) => participant !== name)
+					),
 			},
 			{
-				text: 'Não',
-				style: 'cancel'
-			}
+				text: "Não",
+				style: "cancel",
+			},
 		])
 	}
 
 	return (
 		<View style={styles.container}>
-			<Text style={styles.eventName}>Nome do Evento</Text>
-			<Text style={styles.eventDate}>Terça, 11/10/2022</Text>
+			<Text style={styles.eventName}>React Native + Expo</Text>
+			<Text style={styles.eventDate}>Quarta-Feira, 12/10/2022</Text>
 			<View style={styles.form}>
 				<TextInput
 					style={styles.input}
 					placeholder='Nome do Participante'
 					placeholderTextColor='#6B6B6B'
+					onChangeText={setParticipantName}
+					value={participantName}
 				/>
 				<TouchableOpacity
 					style={styles.button}
@@ -52,7 +66,10 @@ export function Home() {
 				showsVerticalScrollIndicator={false}
 				data={participants}
 				ListEmptyComponent={() => (
-					<Text style={styles.listEmptyText}>Ninguém chegou no evento ainda? Adicione participantes na sua lista de presença.</Text>
+					<Text style={styles.listEmptyText}>
+						Ninguém chegou no evento ainda? Adicione participantes
+						na sua lista de presença.
+					</Text>
 				)}
 				keyExtractor={(item) => item}
 				renderItem={({ item }) => (
@@ -62,7 +79,6 @@ export function Home() {
 						onRemove={() => handleParticipantRemove(item)}
 					/>
 				)}
-
 			/>
 		</View>
 	)
